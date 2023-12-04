4491eed77431e573f3f2d2df95e98c2444387360
asuka-inoue <tohasu081026@gmail.com>
2023-12-04 20:42:15 +0900

    大会向けページ修正

--- js/myscript.js
+++ js/myscript.js
@@ -70,5 +70,17 @@ $(window).on('load scroll', function () {
 	});
 
 });
+
+// スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動。ヘッダーの高さ考慮。)
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    return false;
+  });
+
 // 一番最後に記述
 $("body").removeClass("preload");
